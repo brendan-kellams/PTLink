9c4a31e714316e0a4875ea00e91987e6e1b99ae1
Brendan Kellams <brendan.kellams@gmail.com>
2018-02-05 22:49:28 -0800

    adding components to signup page

--- client/src/Routes/Signup/Signup.js
+++ client/src/Routes/Signup/Signup.js
@@ -21,7 +21,11 @@ class Signup extends Component {
           isUser={this.state.userID}
         />
         <h1>This is the signup page yo!</h1>
+        <h4>Username</h4>
+        <SignUpInput />
         <h4>Email Address</h4>
+        <SignUpInput />
+        <h4>Password</h4>
         <SignUpInput />
       </div>
     )
