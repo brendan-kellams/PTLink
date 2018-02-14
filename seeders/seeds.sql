371743ad77d9bb9b3b8ca89da0dea0399cf52dad
shubha shankar <shankar.shub@gmail.com>
2018-02-13 19:55:04 -0800

    seeders

--- seeders/seeds.sql
+++ seeders/seeds.sql
@@ -2,43 +2,25 @@ use pt_link;
 INSERT INTO Users
   (id,username,password,email,isTeacher,createdAt,updatedAt)
   VALUES
-  (DEFAULT,'Aaron','password123','test@yahoo.com',false,'2018-02-07 03:58:27','2018-02-07 03:58:27');
-INSERT INTO Users
-  (id,username,password,email,isTeacher,createdAt,updatedAt)
-  VALUES
-  (DEFAULT,'Shubha','password123','test1@yahoo.com',false,'2018-02-07 03:58:27','2018-02-07 03:58:27');
-INSERT INTO Users
-  (id,username,password,email,isTeacher,createdAt,updatedAt)
-  VALUES
-  (DEFAULT,'Brendan','password123','test2@yahoo.com',true,'2018-02-07 03:58:27','2018-02-07 03:58:27');
-INSERT INTO Users
-  (id,username,password,email,isTeacher,createdAt,updatedAt)
-  VALUES
-  (DEFAULT,'Andy','password123','test3@yahoo.com',false,'2018-02-07 03:58:27','2018-02-07 03:58:27');
-INSERT INTO Users
-  (id,username,password,email,isTeacher,createdAt,updatedAt)
-  VALUES
-  (DEFAULT,'Bob','password123','test4@yahoo.com',false,'2018-02-07 03:58:27','2018-02-07 03:58:27');
-
-INSERT INTO Communications
-  (id,subject,body,createdAt,updatedAt,senderId)
-  VALUES
-  (DEFAULT,'Test subject','test Body','2018-02-07 04:08:36','2018-02-07 04:08:36',3);
-
-INSERT INTO UserCommunications (unread,createdAt,updatedAt,CommunicationId,UserId) 
-VALUES 
-(true,'2018-02-07 04:08:36','2018-02-07 04:08:36',1,1),
-(true,'2018-02-07 04:08:36','2018-02-07 04:08:36',1,2),
-(true,'2018-02-07 04:08:36','2018-02-07 04:08:36',1,3);
+  (DEFAULT,'ALex Smith','$2a$10$D9nVoRZROL4KXw1otsG0zOVkAy8HWzq06vG9PA3OhO/UBaviai/GO','test1@yahoo.com',false,'2018-02-07 03:58:27','2018-02-07 03:58:27'),
+  (DEFAULT,'Bob Simpson','$2a$10$D9nVoRZROL4KXw1otsG0zOVkAy8HWzq06vG9PA3OhO/UBaviai/GO','test2@yahoo.com',false,'2018-02-07 03:58:27','2018-02-07 03:58:27'),
+  (DEFAULT,'Nick','$2a$10$D9nVoRZROL4KXw1otsG0zOVkAy8HWzq06vG9PA3OhO/UBaviai/GO','test2@yahoo.com',true,'2018-02-07 03:58:27','2018-02-07 03:58:27'),  
+  (DEFAULT,'Susan Lee','$2a$10$D9nVoRZROL4KXw1otsG0zOVkAy8HWzq06vG9PA3OhO/UBaviai/GO','test3@yyahoo.com',true,'2018-02-07 03:58:27','2018-02-07 03:58:27');
 
 
 INSERT INTO Communications
   (id,subject,body,createdAt,updatedAt,senderId)
   VALUES
-  (DEFAULT,'Test subject2','test Body2','2018-02-07 04:08:36','2018-02-07 04:08:36',2);
+  (DEFAULT,'Test subject','test Body','2018-02-07 04:08:36','2018-02-07 04:08:36',3),
+  (DEFAULT,'second subject','second Body','2018-02-07 04:08:36','2018-02-07 04:08:36',4);
 
-INSERT INTO UserCommunications (unread,createdAt,updatedAt,CommunicationId,UserId) 
+
+INSERT INTO SentCommunications (id,unread,createdAt,updatedAt,recipientId,CommunicationId) 
 VALUES 
-(true,'2018-02-07 04:08:36','2018-02-07 04:08:36',2,2),
-(true,'2018-02-07 04:08:36','2018-02-07 04:08:36',2,3),
-(true,'2018-02-07 04:08:36','2018-02-07 04:08:36',2,4);
+(NULL,true,'2018-02-07 04:08:36','2018-02-07 04:08:36',1,2),
+(NULL,true,'2018-02-07 04:08:36','2018-02-07 04:08:36',2,2),
+(NULL,true,'2018-02-07 04:08:36','2018-02-07 04:08:36',3,2),
+(NULL,true,'2018-02-07 04:08:36','2018-02-07 04:08:36',1,1),
+(NULL,true,'2018-02-07 04:08:36','2018-02-07 04:08:36',2,1);
+
+
