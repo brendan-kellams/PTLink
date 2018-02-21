eb659ecaec1bd19502a5097fa7d3b710c591106f
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2018-02-21 10:25:59 -0800

    message style

--- client/src/components/MessageArea/MessageArea.js
+++ client/src/components/MessageArea/MessageArea.js
@@ -8,9 +8,9 @@ const MessageArea = (props) => {
           typeof (props.viewMsg.id) !== 'undefined' ? 
           <div className="message-view">
             <div className="message-id">{props.viewMsg.id} </div>
+            <div className="message-to-user">To: {props.viewMsg.toUser}</div>
+            <div className="message-from-user">From: {props.viewMsg.fromUser}</div>
             <div className="message-title">{props.viewMsg.title}</div>
-            <div className="message-to-user">{props.viewMsg.toUser}</div>
-            <div className="message-from-user">{props.viewMsg.fromUser}</div>
             <div className="message-body"><p>{props.viewMsg.msgBody}</p></div>
           </div> :
           <div>Click on a message to read</div>
