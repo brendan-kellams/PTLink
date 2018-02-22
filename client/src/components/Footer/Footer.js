f762d185ad592c96144e07ed4cb747f235f82189
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2018-02-21 21:36:07 -0800

    public page parallax footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import {footer} from 'react-bootstrap';
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 
 
 const Footer = (props) => {
     return (
-        <footer className='footer'>
-            <div className='container'>
+      <ParallaxProvider>
+          <footer className='footer'>
+            <Parallax
+                  offsetYMax={60}
+                  offsetYMin={-50} >
+                <img className="footer-chars" src="/images/footer-chars.png" />
+                <img className="mobile-footer-chars" src="/images/s_footer-char.png" />
+            </Parallax>    
+            <div className='contact-info'>
                 <span className='text-muted'>{props.text}</span>
                 {props.children}
             </div>
-        </footer>
+          </footer>
+        
+      </ParallaxProvider>
     )
 }
 
